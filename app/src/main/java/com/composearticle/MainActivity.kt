94d2d34cad342f877c2be5d8c51fd8921e0e4aaf
Ahmed Hosny <ahhosnyas@gmail.com>
2023-07-13 06:03:09 +0300

    Initial commit

---
+++ app/src/main/java/com/composearticle/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticle()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeArticlePreview() {
+    ComposeArticleTheme {
+        ComposeArticle()
+    }
+}
+
+@Composable
+fun ComposeArticle() {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    Column {
+        Image(painter = image, contentDescription = "Compose Article")
+
+        Text(
+            text = stringResource(R.string.jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.what_is_jetback_compose),
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+            )
+
+        Text(
+            text = stringResource(R.string.about_tutorial),
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+
+
+
+
+
+
+
